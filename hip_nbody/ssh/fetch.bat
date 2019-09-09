5fed39bfeb95b589cc8c7e1bc7afba723d1dffdf
Dann239 <dann_pavlov@inbox.ru>
2019-09-10 01:25:54 +0300

    removed the ssh folder

--- hip_nbody/ssh/fetch.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-pscp -P 2232 -i key_private.ppk -l pavlov pavlov@rasa202.asuscomm.com:work/data/* ../data/
-pause -> nul
--- hip_nbody/ssh/send.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-pscp -P 2232 -i key_private.ppk -l pavlov ../kernel.cu pavlov@rasa202.asuscomm.com:work/
