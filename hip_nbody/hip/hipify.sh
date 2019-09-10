1e41fbbe27bb278016f1297db88e6e31b0008177
Dann239 <dann_pavlov@inbox.ru>
2019-09-10 04:27:27 +0300

    c++14 arg added to hipcc

--- hip_nbody/hip/hipify.sh
+++ hip_nbody/hip/hipify.sh
@@ -1,6 +1,6 @@
 cp ../main.cpp main.cpp
 cp ../kernel.h kernel.h
 rm a.out
-hipify-clang -o=kernel.cpp ../kernel.cu -stdlib=c++14 &&\
-hipcc main.cpp kernel.cpp &&\
+hipify-clang -o=kernel.cpp ../kernel.cu &&\
+hipcc main.cpp kernel.cpp -stdlib=c++14 &&\
 ./a.out
