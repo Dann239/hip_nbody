885ac17f6746b947df6c0d87d4a9ec3c98b0ba91
Dann239 <dann_pavlov@inbox.ru>
2019-09-10 04:20:15 +0300

    finally we can do constexpr

--- hip_nbody/hip/hipify.sh
+++ hip_nbody/hip/hipify.sh
@@ -2,5 +2,5 @@ cp ../main.cpp main.cpp
 cp ../kernel.h kernel.h
 rm a.out
 hipify-clang -o=kernel.cpp ../kernel.cu &&\
-hipcc main.cpp kernel.cpp &&\
+hipcc main.cpp kernel.cpp -stdlib=c++14 &&\
 ./a.out
