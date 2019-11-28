6305d379fecf280c3d72f676a83df6b67529f192
Dann239 <dann_pavlov@inbox.ru>
2019-11-28 19:59:24 +0300

    hipify script fallback

--- hipify_install.sh
+++ hipify_install.sh
@@ -3,7 +3,7 @@ cd hipify
 
 HIP_R=$(pwd)
 
-mkdir git cmake
+mkdir git cmake build
 cd cmake
 
 export MAKEFLAGS="-j$(nproc)"
@@ -12,7 +12,7 @@ function make_install {
     git clone --depth 1 https://github.com/$2/$1.git $HIP_R/git/$1 --recursive
     mkdir $1
     cd $1
-    cmake -DCMAKE_PREFIX_PATH=~/.local -DCMAKE_INSTALL_PREFIX=~/.local -DCMAKE_BUILD_TYPE=Release $HIP_R/git/$1
+    cmake -DCMAKE_PREFIX_PATH=$HIP_R/build -DCMAKE_INSTALL_PREFIX=$HIP_R/build -DCMAKE_BUILD_TYPE=Release $HIP_R/git/$1
     cmake --build . --target install
     cd ..
 }
@@ -23,5 +23,5 @@ make_install HIP ROCm-Developer-Tools
 
 mkdir hipify
 cd hipify
-cmake -DCMAKE_PREFIX_PATH=~/.local -DCMAKE_INSTALL_PREFIX=~/.local/bin -DCMAKE_BUILD_TYPE=Release $HIP_R/git/HIP/hipify-clang
+cmake -DCMAKE_PREFIX_PATH=$HIP_R/build -DCMAKE_INSTALL_PREFIX=$HIP_R/build/bin -DCMAKE_BUILD_TYPE=Release $HIP_R/git/HIP/hipify-clang
 cmake --build . --target install
